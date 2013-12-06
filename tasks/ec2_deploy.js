fa3bdd1e7a08431b2acb77288a617e3c5cc59d66
Eric Svendsen <eric.svendsen@appliedis.com>
2013-12-06 15:19:16 -0500

    setting local path to cwd when process.cwd() returns a windows-style path

--- tasks/ec2_deploy.js
+++ tasks/ec2_deploy.js
@@ -27,6 +27,9 @@ module.exports = function(grunt){
         var v = grunt.config('pkg.version');
         var dest = util.format(version, v);
         var local = process.cwd();
+		if (local.match(':')) {
+			local = ".";
+		}
 
         var rsync = {
             name: 'v' + v,
